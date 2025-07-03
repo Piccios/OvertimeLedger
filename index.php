87127fbe4fe1a2caf0229424caf68a4cf49a7778
Piccios <lorenzo.hl.picchi@gmail.com>
2025-07-03 12:17:58 +0200

    Remove unused modal styles from index.php to streamline CSS and improve maintainability.

--- index.php
+++ index.php
@@ -451,10 +451,7 @@ function isColorDark($color) {
             background: #fff;
             color: #333;
         }
-        
-        .modal .form-select:not([multiple]):not([size]) {
-            background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23333333' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m1 6 7 7 7-7'/%3e%3c/svg%3e");
-        }
+    
         
         .text-muted {
             color: rgba(255, 255, 255, 0.8) !important;
